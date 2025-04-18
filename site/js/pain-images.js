1537f10463d525ffefc827dade78f46a5c3d8f79
Maxim <levko.maxim@gmail.com>
2025-04-18 18:27:41 +0300

    carousels

---
+++ site/js/pain-images.js
@@ -0,0 +1,11 @@
+// List of all pain images
+const PAIN_IMAGES = [
+  {
+    "path": "../assets/pains/1.png",
+    "alt": "Проблемы здоровья сотрудников в рабочей среде"
+  },
+  {
+    "path": "../assets/pains/2.png",
+    "alt": "Последствия отсутствия профилактики здоровья на рабочем месте"
+  }
+];
---
+++ site/js/worker-images.js
@@ -0,0 +1,19 @@
+// List of all worker images
+const WORKER_IMAGES = [
+  {
+    "path": "../assets/workers/1_1work.png",
+    "alt": "Сотрудники выполняют упражнения на рабочем месте"
+  },
+  {
+    "path": "../assets/workers/1_2routine.png",
+    "alt": "Повседневная гимнастика для сотрудников"
+  },
+  {
+    "path": "../assets/workers/2_1work.png",
+    "alt": "Профессиональная гимнастика для сотрудников"
+  },
+  {
+    "path": "../assets/workers/2_2routine.png",
+    "alt": "Профессиональная гимнастика для сотрудников"
+  }
+];
